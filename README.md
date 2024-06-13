3506299cc7a5f8d397f9cf17d0b755db9a3b0fd4
Joseph Guaneri <joe@guaneri.com>
2024-06-13 13:32:35 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+This module create an EC2 instance role that is SSM enabled.  The role is named EC2Role.
+
+Prerequisites:
+This module requires the following VPC endpoints to be enabled:
+com.amazonaws.us-east-1.s3
+com.amazonaws.us-east-1.ssm
+com.amazonaws.us-east-1.ec2messages
+com.amazonaws.us-east-1.ssmmessages
---
+++ main.tf
@@ -0,0 +1,25 @@
+data "aws_iam_policy_document" "instance-assume-role-policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+resource aws_iam_role "ec2_role" {
+  name               = "EC2Role"
+
+  assume_role_policy = data.aws_iam_policy_document.instance-assume-role-policy.json
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role",
+    "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+  ]
+}
+
+resource aws_iam_instance_profile "ec2_instance_profile" {
+  name = "EC2Role"
+  role = aws_iam_role.ec2_role.name
+}
